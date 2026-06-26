ef6d84d469564b0b4febe7c81bb954292d9f77fb
sashadonh <vitakor824@gmail.com>
2026-06-27 00:54:22 +0300

    Create settings.lua

---
+++ src/settings.lua
@@ -0,0 +1,21 @@
+-- Настройки MiniAtlas
+Settings = {
+    AutoFarm = true,
+    AutoQuest = true,
+    AutoCollect = true,
+    AutoSprinkler = true,
+    AutoDig = true,
+    SelectedField = "Sunflower Field",
+    FarmRadius = 30,
+    TweenSpeed = 0.85,
+    CollectDelay = 0.35,
+}
+
+Fields = {
+    "Sunflower Field", "Mushroom Field", "Dandelion Field", "Clover Field",
+    "Blue Flower Field", "Bamboo Field", "Strawberry Field", "Pineapple Patch",
+    "Rose Field", "Spider Field", "Cactus Field", "Pumpkin Patch",
+    "Coconut Field", "Pepper Patch", "Mountain Top Field", "Stump Field"
+}
+
+print("⚙️ Настройки MiniAtlas загружены")
