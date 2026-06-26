2461007d661de40a05d8c14c4fc5a426e14e4de2
sashadonh <vitakor824@gmail.com>
2026-06-27 01:08:34 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,9 @@
--- MiniAtlas Loader by sashadonh
-print("🔄 Загрузка MiniAtlas v2...")
+-- MiniAtlas Loader v2.3 by sashadonh
+print("🔄 Загрузка MiniAtlas v2.3...")
 
 local repo = "https://raw.githubusercontent.com/sashadonh/miniatlas/main/"
 
-loadstring(game:HttpGet(repo .. "src/main.lua", true))()
+-- Анти-кэш (чтобы всегда брал свежую версию)
+local nocache = "?t=" .. os.time()
+
+loadstring(game:HttpGet(repo .. "src/main.lua" .. nocache, true))()
