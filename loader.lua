acccc246289918d995b5b2a6ef436d5ecd99e331
sashadonh <vitakor824@gmail.com>
2026-06-27 01:04:03 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 -- MiniAtlas Loader by sashadonh
-print("🔄 Загрузка MiniAtlas BSS...")
+print("🔄 Загрузка MiniAtlas v2...")
 
 local repo = "https://raw.githubusercontent.com/sashadonh/miniatlas/main/"
 
-loadstring(game:HttpGet(repo .. "src/main.lua"))()
+loadstring(game:HttpGet(repo .. "src/main.lua", true))()
